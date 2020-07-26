7fc4cc6d59fd00902a0ebaf2bab61eb8317e6c62
Eduardo Quiroga <ed.quiroga.2103@gmail.com>
2020-07-25 18:37:11 -0600

    [Edit] Renamed Python Modules

--- Web Service/db_classes.py
+++ Web Service/db_classes.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import datetime
 from functools import wraps
-from UserServer import app
+from ce_labs_api import app
 
 db = SQLAlchemy(app)
 
