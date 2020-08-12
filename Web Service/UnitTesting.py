35c54232ae934a23c9da554e9b9f9b5047260bad
Eduardo Quiroga <ed.quiroga.2103@gmail.com>
2020-08-12 15:11:51 -0600

    [Update] Unit Testing

--- Web Service/UnitTesting.py
+++ Web Service/UnitTesting.py
@@ -3,6 +3,19 @@ import requests
 from flask import jsonify
 from requests.auth import HTTPBasicAuth
 
+
+def get_token():
+    r = requests.post('http://127.0.0.1:5001/login', 
+        headers={
+                'Authorization': 'Basic QWRtaW46MTIzNDU=', 
+                'Content-Type': 'application/json', 
+                } 
+        ,auth=HTTPBasicAuth('Op', 'Op'))
+
+    token = r.json()['token']
+    return token
+
+
 class TestStringMethods(unittest.TestCase):
 
     def test_good_login(self):
@@ -25,6 +38,19 @@ class TestStringMethods(unittest.TestCase):
                 } 
         ,auth=HTTPBasicAuth('Op', 'Error'))
         self.assertEqual('<Response [401]>', str(r))        
+
+    def test_get_reservation(self):
+        token = get_token()
+
+        r = requests.get('http://127.0.0.1:5001/reservation', 
+        headers={
+                'Authorization': 'Basic QWRtaW46MTIzNDU=', 
+                'Content-Type': 'application/json', 
+                'x-access-token': token,
+                } 
+        )
+        self.assertEqual('<Response [200]>', str(r))        
+
 
 
 if __name__ == '__main__':
